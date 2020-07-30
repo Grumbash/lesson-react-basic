a042cb90b9642b8f072513c96e82592ed071a8b7
Gleb Rosev <rossevgleb@gmail.com>
2020-07-30 21:58:53 +0300

    Added forms

--- src/App.js
+++ src/App.js
@@ -1,62 +1,54 @@
 import React from "react";
-import axios from "axios";
-
-import User from "./components/User";
-
-const api = "https://5f22f8cf0e9f660016d88b17.mockapi.io/";
-const usersAPI = `${api}users`;
+// import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: [],
-      warning: "",
+      userName: "",
+      userPassword: "",
+      userCustomField: "",
+      value: "grapefruit",
     };
   }
 
   componentDidMount() {
-    axios.get(usersAPI).then(({ data }) => {
-      this.setState({ users: data });
+    this.setState({
+      userName: "Gleb",
+      userPassword: "lolkek",
     });
   }
 
-  componentDidUpdate() {
-    if (this.state.users.length < 6 && this.state.users.length !== 0) {
-      this.setState({ warning: "You have deleted a lot of users!!!" });
-    }
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log(this.state, nextState);
-    if (this.state.users.length === nextState.users.length) {
-      return false;
-    }
-    return true;
-  }
-
-  handleClick = (e) => {
-    const { users } = this.state;
-    const newUsers = [...users];
-    newUsers.pop();
-    this.setState({ users: newUsers });
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
+  handleChangeSelect = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  componentDidUpdate() {
+    console.table(this.state);
+  }
+
   render() {
-    const { users, warning } = this.state;
     return (
       <>
-        <button onClick={this.handleClick}>Delete last user</button>
-        <div>Users count: {users.length}</div>
-        {!!warning && <p className="title">{warning}</p>}
-
-        <ul>
-          {users.length > 0
-            ? users.map((user) => (
-                <User key={user.id} name={user.name} avatar={user.avatar} />
-              ))
-            : "no users found"}
-        </ul>
+        <input
+          name="userName"
+          value={this.state.userName}
+          onChange={this.handleChange}></input>
+        <input name="userCustomField" onChange={this.handleChange}></input>
+        <select value={this.state.value} onChange={this.handleChangeSelect}>
+          <option value="grapefruit">Грейпфрут</option>
+          <option value="lime">Лайм</option>
+          <option value="coconut">Кокос</option>
+          <option value="mango">Манго</option>
+        </select>
+        <input
+          name="userPassword"
+          value={this.state.userPassword}
+          onChange={this.handleChange}></input>
       </>
     );
   }
